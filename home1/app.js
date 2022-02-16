13dcb99dfcf144fc0edf6806f35b7e0c008edee6
OlegSaikevych <olegsay@outlook.com>
2022-02-16 19:18:52 +0200

    end

--- home1/app.js
+++ home1/app.js
@@ -9,74 +9,73 @@ const fs = require('fs');
 // і створити файли txt в папках (online, inPerson) в яких як дату покласти юзерів з ваших масивів, але щоб ваш файл виглядав як NAME: ім'я з обєкту і т.д і всі пункти з нового рядка.
 //
 // Коли ви це виконаєте напишіть функцію яка буде міняти місцями юзерів з одного файлу і папки в іншу. (ті, що були в папці inPerson будуть в папці online)
-// const userOnline = [
-//     {
-//         name: "Andrii",
-//         age: 22,
-//         city: "Lviv"
-//     }]
-// const inPesonUser = [{
-//     name: "Roman",
-//     age: 45,
-//     city: "Odesa"
-// }]
-//
-// fs.mkdir(path.join(__dirname, 'main'), (err) => {
-//     if (err) {
-//         console.log(err);
-//         throw err;
-//     }
-//     fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
-//         if (err) {
-//             console.log(err);
-//             throw err;
-//         }
-//         fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
-//             if (err) {
-//                 console.log(err);
-//                 throw err;
-//             }
-//             for (const user of userOnline) {
-//                 fs.writeFile(path.join(__dirname, 'main', 'online', 'userOnline.txt'), `Name:${user.name}\nAge:${user.age}\nCiti:${user.city}\n`, (err) => {
-//                     if (err) {
-//                         console.log(err);
-//                         throw err;
-//                     }
-//                     for (const person of inPesonUser) {
-//                         fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'userInPerson.txt'), `Name:${person.name}\nAge:${person.age}\nCiti:${person.city}\n`, (err) => {
-//                             if (err) {
-//                                 console.log(err);
-//                                 throw err;
-//                             }
-//                         })
-//                     }
-//                 })
-//             }
-//
-//         })
-//         })
-//     })
+const userOnline = [
+    {
+        name: "Andrii",
+        age: 22,
+        city: "Lviv"
+    }];
+const inPesonUser = [{
+    name: "Roman",
+    age: 45,
+    city: "Odesa"
+}];
 
+fs.mkdir(path.join(__dirname, 'main'), (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            for (const user of userOnline) {
+                fs.writeFile(path.join(__dirname, 'main', 'online', 'userOnline.txt'), `Name:${user.name}\nAge:${user.age}\nCiti:${user.city}\n`, (err) => {
+                    if (err) {
+                        console.log(err);
+                        throw err;
+                    }
+                    for (const person of inPesonUser) {
+                        fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'userInPerson.txt'), `Name:${person.name}\nAge:${person.age}\nCiti:${person.city}\n`, (err) => {
+                            if (err) {
+                                console.log(err);
+                                throw err;
+                            }
+                        });
+                    }
+                });
+            }
+
+        });
+    });
+});
 
 
 fs.readFile(path.join(__dirname, 'main', 'online', 'userOnline.txt'), 'utf-8', (err, data) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     }
     fs.appendFile(path.join(__dirname, 'main', 'inPerson', 'userInPerson.txt'), `${data}`, {flag: 'w'}, (err) => {
         if (err) {
-            console.log(err)
+            console.log(err);
         }
-    })
-})
+    });
+});
 
 fs.readFile(path.join(__dirname, 'main', 'inPerson', 'userInPerson.txt'), 'utf-8', (err, data) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     }
     fs.appendFile(path.join(__dirname, 'main', 'online', 'userOnline.txt'), `${data}`, {flag: 'w'}, (err) => {
         if (err) {
-            console.log(err)
+            console.log(err);
         }
-    })
-})
+    });
+});
